e41807eaa60d0e9dcf01705524fafcf8b6be7178
JoBoCl <1917489+JoBoCl@users.noreply.github.com>
2025-12-03 16:57:40 +0100

    Day 02 complete

--- src/day02.rs
+++ src/day02.rs
@@ -1,8 +1,13 @@
 extern crate test;
 
+use std::ops::RangeInclusive;
+
+use itertools::Itertools;
 use solver::{Solver, SolverToAny};
 
-pub struct Day02 {}
+pub struct Day02 {
+    ranges: Vec<RangeInclusive<u64>>,
+}
 
 impl SolverToAny for Day02 {
     fn as_any(&self) -> &dyn std::any::Any {
@@ -11,18 +16,80 @@ impl SolverToAny for Day02 {
 }
 
 impl Day02 {
-    pub fn try_create(_input: Box<dyn Iterator<Item = String>>) -> anyhow::Result<Box<dyn Solver>> {
-        Ok(Box::new(Day02 {}))
+    pub fn try_create(input: Box<dyn Iterator<Item = String>>) -> anyhow::Result<Box<dyn Solver>> {
+        let mut ranges = Vec::new();
+        for line in input {
+            for group in line.split(',') {
+                if let Some((l, r)) = group.split_once('-') {
+                    let left = l.parse::<u64>();
+                    let right = r.parse::<u64>();
+                    match (left, right) {
+                        (Ok(pl), Ok(pr)) => ranges.push(pl..=pr),
+                        (l, r) => {
+                            panic! {"could not parse {l:?} or {r:?}"}
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(Box::new(Day02 { ranges }))
+    }
+
+    // 10..100 => 11, 100..1000 => None, 1000..10000 => 101
+    fn multiplier(number: &u64) -> Option<u64> {
+        let length = number.ilog10(); // 10..100 => 1, 100..1000 => 2, 1000..10000 => 4
+        if length % 2 == 0 {
+            return None;
+        }
+        Some(10_u64.pow(1 + length / 2) + 1)
+    }
+
+    fn repeated_segment(number: &u64) -> bool {
+        if let Some(mul) = Day02::multiplier(number) {
+            number % mul == 0
+        } else {
+            false
+        }
+    }
+
+    fn repeated_segments(number: &u64) -> bool {
+        let s = number.to_string();
+        let l = s.len();
+        for i in 1..=(l / 2) {
+            if l % i != 0 {
+                continue;
+            }
+            let chunks = s.chars().chunks(i);
+            if chunks
+                .into_iter()
+                .map(|c| c.collect::<String>())
+                .all_equal()
+            {
+                return true;
+            }
+        }
+        false
     }
 }
 
 impl Solver for Day02 {
     fn part_one(&self) -> anyhow::Result<String> {
-        Err(anyhow::anyhow! {"Not Implemented yet"})
+        Ok(self
+            .ranges
+            .iter()
+            .flat_map(|range| range.clone().into_iter().filter(Day02::repeated_segment))
+            .sum::<u64>()
+            .to_string())
     }
 
     fn part_two(&self) -> anyhow::Result<String> {
-        Err(anyhow::anyhow! {"Not Implemented yet"})
+        Ok(self
+            .ranges
+            .iter()
+            .flat_map(|range| range.clone().into_iter().filter(Day02::repeated_segments))
+            .sum::<u64>()
+            .to_string())
     }
 }
 
@@ -40,7 +107,7 @@ mod tests {
             .map(String::from);
 
         let solver = Day02::try_create(Box::new(input)).unwrap();
-        assert! {solver.part_one().is_err()};
+        assert_eq! {solver.part_one()?, "1227775554"};
         Ok(())
     }
 
@@ -51,7 +118,7 @@ mod tests {
             .map(String::from);
 
         let solver = Day02::try_create(Box::new(input)).unwrap();
-        assert! {solver.part_two().is_err()};
+        assert_eq! {solver.part_two()?, "4174379265"};
         Ok(())
     }
 
@@ -62,8 +129,8 @@ mod tests {
             .map(String::from);
 
         let solver = Day02::try_create(Box::new(input)).unwrap();
-        assert! {solver.part_one().is_err()};
-        assert! {solver.part_two().is_err()};
+        assert_eq! {solver.part_one()?, "40398804950"};
+        assert_eq! {solver.part_two()?, "65794984339"};
         Ok(())
     }
 
