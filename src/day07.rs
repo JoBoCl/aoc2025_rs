8885206f9ec6152377c182d33ff45e6425135ed0
Joshua Borkowski-Clark <1917489+JoBoCl@users.noreply.github.com>
2025-12-07 21:42:19 +0100

    Day07 complete

--- src/day07.rs
+++ src/day07.rs
@@ -1,8 +1,45 @@
 extern crate test;
 
+use anyhow::bail;
+use itertools::Itertools;
 use solver::{Solver, SolverToAny};
+use std::collections::{HashMap, HashSet, BTreeSet};
+use std::fmt::{Display, Formatter};
 
-pub struct Day07 {}
+pub struct Day07 {
+    map: Vec<Vec<Space>>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Space {
+    Empty,
+    Splitter,
+    Source,
+}
+
+impl TryFrom<char> for Space {
+    type Error = anyhow::Error;
+
+    fn try_from(c: char) -> anyhow::Result<Self> {
+        match c {
+            'S' => Ok(Space::Source),
+            '^' => Ok(Space::Splitter),
+            '.' => Ok(Space::Empty),
+            _ => bail! {"could not match {c:?}"},
+        }
+    }
+}
+
+impl Display for Space {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let c = match self {
+            Space::Empty => '.',
+            Space::Splitter => '^',
+            Space::Source => 'S',
+        };
+        write! {f, "{}", c}
+    }
+}
 
 impl SolverToAny for Day07 {
     fn as_any(&self) -> &dyn std::any::Any {
@@ -11,18 +48,95 @@ impl SolverToAny for Day07 {
 }
 
 impl Day07 {
-    pub fn try_create(_input: Box<dyn Iterator<Item = String>>) -> anyhow::Result<Box<dyn Solver>> {
-        Ok(Box::new(Day07 {}))
+    pub fn try_create(input: Box<dyn Iterator<Item = String>>) -> anyhow::Result<Box<dyn Solver>> {
+        Ok(Box::new(Day07 {
+            map: input
+                .map(|line| line.chars().map(|c| Space::try_from(c)).try_collect())
+                .try_collect()?,
+        }))
     }
 }
 
 impl Solver for Day07 {
     fn part_one(&self) -> anyhow::Result<String> {
-        Err(anyhow::anyhow! {"Not Implemented yet"})
+        let mut beams = HashSet::new();
+        let Some(x) = self.map[0].iter().position(|s| *s == Space::Source) else {
+            panic! {"first line did not have start point"}
+        };
+        beams.insert(x);
+
+        let mut splits = 0;
+        let mut empty_only = false;
+        for row in self.map.iter().skip(1) {
+            empty_only = !empty_only;
+            let mut new_beams = HashSet::new();
+            for beam in beams {
+                match row.get(beam) {
+                    Some(Space::Source) => panic! {"should only have a single source"},
+                    Some(Space::Splitter) => {
+                        splits += 1;
+                        new_beams.insert(beam - 1);
+                        new_beams.insert(beam + 1);
+                    }
+                    Some(Space::Empty) => {
+                        new_beams.insert(beam);
+                    }
+                    None => {}
+                }
+            }
+            beams = new_beams;
+        }
+
+        Ok(splits.to_string())
     }
 
     fn part_two(&self) -> anyhow::Result<String> {
-        Err(anyhow::anyhow! {"Not Implemented yet"})
+        let mut routes_to: HashMap<(usize, usize), usize> = HashMap::new();
+
+        let mut beams = BTreeSet::new();
+        let Some(x) = self.map[0].iter().position(|s| *s == Space::Source) else {
+            panic! {"first line did not have start point"}
+        };
+        beams.insert(x);
+        routes_to.insert((x, 0_usize), 1);
+
+        for (y, row) in self.map.iter().enumerate().skip(1) {
+            let mut new_beams = BTreeSet::new();
+            for beam in beams {
+                match row.get(beam) {
+                    Some(Space::Source) => panic! {"should only have a single source"},
+                    Some(Space::Splitter) => {
+                        let above = routes_to[&(beam, y - 1)];
+                        if !new_beams.insert(beam - 1) {
+                            let left = routes_to[&(beam - 1, y)];
+                            routes_to.insert((beam - 1, y), left + above);
+                        } else {
+                            routes_to.insert((beam - 1, y), above);
+                        }
+                        new_beams.insert(beam + 1);
+                        routes_to.insert((beam + 1, y), above);
+                    }
+                    Some(Space::Empty) => {
+                        new_beams.insert(beam);
+                        let prior = routes_to.get(&(beam,y)).unwrap_or(&0);
+                        routes_to.insert((beam, y), prior + routes_to[&(beam, y - 1)]);
+                    }
+                    None => {
+                        routes_to.insert((beam, y), routes_to[&(beam, y - 1)]);
+                    }
+                }
+            }
+            beams = new_beams;
+        }
+
+        let mut total = 0;
+        for ((_, y), n) in routes_to {
+            if y == self.map.len() - 1 {
+                total += n;
+            }
+        }
+
+        Ok(total.to_string())
     }
 }
 
@@ -40,7 +154,7 @@ mod tests {
             .map(String::from);
 
         let solver = Day07::try_create(Box::new(input)).unwrap();
-        assert! {solver.part_one().is_err()};
+        assert_eq! {solver.part_one()?, "21"};
         Ok(())
     }
 
@@ -51,7 +165,7 @@ mod tests {
             .map(String::from);
 
         let solver = Day07::try_create(Box::new(input)).unwrap();
-        assert! {solver.part_two().is_err()};
+        assert_eq! {solver.part_two()?, "40"};
         Ok(())
     }
 
@@ -62,8 +176,8 @@ mod tests {
             .map(String::from);
 
         let solver = Day07::try_create(Box::new(input)).unwrap();
-        assert! {solver.part_one().is_err()};
-        assert! {solver.part_two().is_err()};
+        assert_eq! {solver.part_one()?, "1592"};
+        assert_eq! {solver.part_two()?, "17921968177009"};
         Ok(())
     }
 
